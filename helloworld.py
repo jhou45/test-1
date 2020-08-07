88b56f3742125bafc83d11dddc14c7e36011db20
jhou45 <42850230+jhou45@users.noreply.github.com>
2020-08-07 10:25:11 -0700

    Update helloworld.py

--- helloworld.py
+++ helloworld.py
@@ -1,11 +1,6 @@
 import sys
-from git import Repo
-
-f = open('README.md')
-print(f.readline())
 
 arg = sys.argv[1]
-print('hello world')
-print(arg)
+print('hello world {}'.format(arg))
 
 print('HEREEE')
